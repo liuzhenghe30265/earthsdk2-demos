37203c09c5f2751872fc195a719e2e2b51e5c54c
liuzhenghe <liuzhenghe@ikingtec.com>
2024-04-28 16:01:33 +0800

    fix: update

---
+++ js/earthsdk.js
@@ -0,0 +1,11 @@
+document.write(`\<script>
+const xe2BaseDir = '../../../../js/';
+window.xbsjDirs = window.xbsjDirs || { xe2BaseDir };
+<\/script>
+<script src="../../../../js/xbsj-xe2/dist-web/xe2-all-in-one.js"><\/script>
+<script src="../../../../js/xbsj-xe2-assets/dist-web/xe2-assets.js"><\/script>
+<script src="../../../../js/vue-xe2-plugin/dist-web/vue-xe2-plugin.js"><\/script>
+<script src="../../../../js/smplotting-xe2-plugin/dist-web/smplotting-xe2-plugin.js"><\/script>
+<script src="../../../../js/esobjs-xe2-plugin-assets/dist-web/esobjs-xe2-plugin-assets.js"><\/script>
+<script src="../../../../js/esobjs-xe2-plugin/dist-web/esobjs-xe2-plugin.js"><\/script>
+`);
